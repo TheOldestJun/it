76dc6e7ee7e6e01727faf7ed5039a50d62cf41fe
Stanislav <s.v.pukha@gmail.com>
2024-10-18 16:38:34 +0300

    user creaton init

---
+++ app/api/register/route.js
@@ -0,0 +1,6 @@
+import prisma from "@/prisma";
+import bcrypt from "bcrypt";
+
+export async function POST(request) {
+    const body = await request.json();
+}
--- components/adminPanel/createForm.jsx
+++ components/adminPanel/createForm.jsx
@@ -1,36 +1,80 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { useState } from "react";
 
 
 export default function CreateForm() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [roleId, setRoleId] = useState('');
+
     return (
         <div className='flex justify-center align-middle py-12 sm:px-6 lg:px-8'>
-            <form action="">
-                <div class="grid grid-cols-12 gap-2 max-w-2xl">
-                    <div class="col-span-6">
-                        <Label htmlFor="email">Електронна пошта</Label>
-                        <Input id="email" type="email" />
-                    </div>
-                    <div class="col-span-6">
-                        <Label htmlFor="password">Пароль</Label>
-                        <Input id="password" type="password" />
-                    </div>
-                    <div class="col-span-6">
-                        <Label htmlFor="name">Ім'я користувача</Label>
-                        <Input id="name" type="text" />
-                    </div>
-                    <div class="col-span-6">
-                        <Label htmlFor="surname">Прізвище користувача</Label>
-                        <Input id="surname" type="text" />
-                    </div>
-                    <div class="col-span-6">
-                        <Label htmlFor="phone">Телефон</Label>
-                        <Input id="phone" type="text" />
-                    </div>
+            <div class="grid grid-cols-12 gap-2 border-gray-400 bg-white rounded-sm">
+                <div class="col-span-6">
+                    <Label htmlFor="email">Електронна пошта</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
-
-            </form>
+                <div class="col-span-6">
+                    <Label htmlFor="password">Пароль</Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div class="col-span-6">
+                    <Label htmlFor="name">Ім'я користувача</Label>
+                    <Input
+                        id="name"
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </div>
+                <div class="col-span-6">
+                    <Label htmlFor="surname">Прізвище користувача</Label>
+                    <Input
+                        id="surname"
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div class="col-span-12">
+                    <Select onValueChange={() => setRoleId(value)}>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Тип користувача" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="DIRECTORS">Директорат</SelectItem>
+                            <SelectItem value="APPLICANTS">Заявники</SelectItem>
+                            <SelectItem value="EXECUTORS">Виконавці</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div class="col-span-12">
+                    <Button type="submit" className="w-full">Створити</Button>
+                </div>
+            </div>
         </div>
     );
 }
